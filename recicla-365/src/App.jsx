cb519e2871ac9081562f407e30dc89685690f884
Douglas Graça <159181176+DougBrando@users.noreply.github.com>
2025-08-27 17:58:18 -0300

    Implementado as paginas e o roteamento

--- recicla-365/src/App.jsx
+++ recicla-365/src/App.jsx
@@ -1,11 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Index from "./pages/Login/Index";
 
-function App() {
-
+export default function App() {
   return (
     <>
+      <Routes>
+      <Route path="/" element={<Index />} />
+        
       
-    </>
+      </Routes>
+  </>
   )
 }
-
-export default App
